449aa6ad6da55534f3ce319429a2c7c856f5deb8
Alfian Soasiu <55717248+AlfianSoasiu@users.noreply.github.com>
2019-11-08 00:05:20 +0700

    Add files via upload

---
+++ PBO1-10118001-Latihan53-Rabbit/build/classes/Rabbit.rs
@@ -0,0 +1,1 @@
+pbo3.pkg10117111.latihan53.rabbit.Rabbit
---
+++ PBO1-10118001-Latihan53-Rabbit/src/outputRabbit.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author 
+ * Nama      : Alfian Soasiu
+ * Kelas     : PBO1
+ * NIM       : 10118001
+ * Deskripsi : Menampilkan output rabbit
+ */
+public class outputRabbit {
+
+    public static void main(String[] args) {
+        
+        Rabbit rabbit = new Rabbit("Peter",false, "grass", 4, "grey" );
+        System.out.println("Hello, His name is "+rabbit.getName());
+        System.out.println(rabbit.getName()+" is Vegetarian? "+rabbit.isVegetarian());
+        System.out.println(rabbit.getName()+" eats "+rabbit.getEats());
+        System.out.println(rabbit.getName()+" has "+rabbit.getNoOfLegs()+" legs.");
+        System.out.println(rabbit.getName()+" color is "+rabbit.getColor());
+    }
+    
+}
